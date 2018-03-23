3442581119a920c9145ff64732d545028aa2b8c6
Mike C <mikecronin92@gmail.com>
2018-03-23 20:19:50 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,10 @@
 def my_select(collection)
   i = 0
+  new_collection
   while i < collection.length
-    new_val = yield(array[i])
+    new_val = yield(collection[i])
     if new_val == true
-      return array[i]
+      new_collection << collection[i]
     end 
     i = i + 1
   end
